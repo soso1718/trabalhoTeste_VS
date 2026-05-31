f9c6afbb97c08894bfab7b73716cb366652db211
camila.silva <camila.silva314@aluno.ce.gov.br>
2026-05-31 15:25:03 -0300

    colocando as insformações do env da maneira correta

--- pages/login-page.ts
+++ pages/login-page.ts
@@ -1,12 +1,13 @@
 import type { Page } from '@playwright/test';
 import { expect } from '@playwright/test';
+import { BASE_URL } from '../config';
 export class LoginPage {
     readonly page: Page;
 constructor(page: Page) {
         this.page = page;
     }
 async login(email: string, password: string) {
-        await this.page.goto('https://studylab.free.laravel.cloud/login');
+        await this.page.goto(`${BASE_URL}/login`);
         await this.page.fill('input[type="email"]', email);
         await this.page.fill('input[type="password"]', password);
         await this.page.click('button[type="submit"]');
