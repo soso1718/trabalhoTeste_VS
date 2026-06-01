52a5aa80a7e2727789a7d19bbc94fc212c753767
soso1718 <anasophiasantiago679@gmail.com>
2026-05-31 21:36:07 -0300

    feat: criação do CRUD de matérias, com 2 casos felizes, 2 tristes e 2 de borda

--- pages/SubjectsPage.ts
+++ pages/SubjectsPage.ts
@@ -2,16 +2,33 @@ import {Page} from '@playwright/test';
 import { BASE_URL } from '../config';
 
 export class SubjectsPage {
-    readonly page: Page;
-    readonly url: string;
+    constructor(private page: Page) {}
 
-    constructor(page: Page) {
-        this.page = page;
-        this.url = `${BASE_URL}/subjects`;
+    async openSubjects() 
+    {
+        await this.page.getByRole('link', { name: 'Matérias Matérias' }).click();
+        await this.page.getByRole('link', { name: 'Ver matérias' }).click();
     }
 
-    async goto() {
-        await this.page.goto(this.url);
+    async addSubjectSucesso() {
+        await this.page.getByRole('button', { name: 'Adicionar matéria' }).click();
+        await this.page.locator('#modalSubjectName').selectOption('Redação');
+        await this.page.getByRole('textbox', { name: 'Ex: Prof. João Silva' }).fill('Maria');
+        await this.page.locator('#modalSubjectSemester').selectOption('5');
+        await this.page.getByRole('button', { name: 'Salvar matéria' }).click();
     }
 
+    async editSubjectSucesso() {
+        await this.page.getByRole('button', { name: 'Editar' }).nth(1).click();
+        await this.page.getByRole('textbox', { name: 'Ex: Prof. João Silva' }).fill('João');
+        await this.page.getByRole('button', { name: 'Salvar alterações' }).click();
+    }
+
+
+    async addSubjectSemProfessor() {   
+        await this.page.getByRole('button', { name: 'Adicionar matéria' }).click();
+        await this.page.locator('#modalSubjectName').selectOption('Biologia');
+        await this.page.locator('#modalSubjectSemester').selectOption('8');
+        await this.page.getByRole('button', { name: 'Salvar matéria' }).click();
+    }
 }
