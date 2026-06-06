3ed79d95668b835476af88bc4e360e00f77dd279
camila.silva <camila.silva314@aluno.ce.gov.br>
2026-06-06 12:03:22 -0300

    chore: remove o retries: 1 e deixa padrão

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], 
+      use: { ...devices['Desktop Chrome'],
             
       },
     },
